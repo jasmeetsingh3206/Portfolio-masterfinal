7d09b160445c94636dc3d5831ac4e5f9ebd10fb5
Jasmeet <62832253+jasmeetsingh3206@users.noreply.github.com>
2023-04-28 23:55:17 +0530

    justify center

--- src/components/Projects/newProjectCard.jsx
+++ src/components/Projects/newProjectCard.jsx
@@ -18,7 +18,7 @@ export default function NewCard() {
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">TransService WebDashboard</h5>
                 <p className="mb-3 font-normal text-gray-700">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
             </div>
-            <div className="flex flex-row gap-3 m-2 mb-4 justify-around">
+            <div className="flex flex-row gap-3 m-2 mb-4 justify-center">
                 <button type="button" onClick={handleClick} class="text-white hover:bg-purple-600 shadow-sm bg-purple-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-lg text-xl px-3 py-0 h-12 text-center inline-flex items-center ">
                     <GoRepo className="mr-1" /> GitHub
                 </button>
