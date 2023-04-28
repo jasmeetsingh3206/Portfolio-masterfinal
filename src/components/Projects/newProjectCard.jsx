3ff8f5e286a28d7a5bcbb321fc2ec059e0b2b817
Jasmeet <62832253+jasmeetsingh3206@users.noreply.github.com>
2023-04-28 22:42:34 +0530

    dependencies updated

--- src/components/Projects/newProjectCard.jsx
+++ src/components/Projects/newProjectCard.jsx
@@ -19,7 +19,7 @@ export default function NewCard() {
                 <p className="mb-3 font-normal text-gray-700">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
             </div>
             <div className="flex flex-row gap-1 m-2 mb-4">
-            <button type="button" class="text-white  bg-blue-700 hover:bg-blue-100 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <button type="button" onClick={handleClick} class="text-white  bg-blue-700 hover:bg-blue-100 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 <GoRepo className="mr-1" /> GitHub
             </button>
             <button type="button" class="text-white  bg-blue-700 hover:bg-blue-100 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
