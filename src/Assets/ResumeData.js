314f8906d6810a726e77361be8e0e7675a920877
Jasmeet <62832253+jasmeetsingh3206@users.noreply.github.com>
2023-04-30 15:34:59 +0530

    updated resume data

--- src/Assets/ResumeData.js
+++ src/Assets/ResumeData.js
@@ -18,7 +18,9 @@ export const user= {
         desc:"Consumer facing web dashboard on VUE.js, with ablity to filter jobs on basis of branch, client and site, displaying interactive charts and pointers on a live map. Linking it with existing .NET services via an API",
         tech:["Vue.js", "Pinia", "Axios", "Postgres", "Tailwind", "Leaflet", "Typescript"],
         demolink: "https://jobdashboard3206.netlify.app/",
-        githublink: "https://github.com/jasmeetsingh3206/WebDashboard-2.0"
+        githublink: "https://github.com/jasmeetsingh3206/WebDashboard-2.0",
+        imgsrc:"https://i.ibb.co/TMZCkBM/Screenshot-2023-04-30-at-15-19-29.jpg"
+
     },
     {
         id:2,
@@ -26,7 +28,9 @@ export const user= {
         desc:"Employe management portal with activity logs, employee status, leaves management, a dashboard to view other employees of the organisation and their statuses.  ",
         tech:["Vue.js", "Bootstrap", "Axios", "Web api", "Tailwind", "Jwt"],
         demolink: "https://fantastic-pixie-0d8d92.netlify.app",
-        githublink: "https://github.com/jasmeetsingh3206/hellovue"
+        githublink: "https://github.com/jasmeetsingh3206/hellovue",
+        imgsrc:"https://i.ibb.co/h99dRCQ/Screenshot-2023-04-30-at-15-32-45.jpg"
+
     },
     {
         id:3,
@@ -34,7 +38,8 @@ export const user= {
         desc:"A live content-based movie recommender system that utilizes the TMDB 5000 MOVIE DATASET to match inputted movie names with its database. The system uses cosine similarity to determine the 10 most similar movies to recommend to the user.",
         tech:["Python", "Pandas", "Numpy", "Streamlit", "Coscine similarity"],
         demolink: "https://pingpongg.netlify.app/",
-        githublink: "https://github.com/jasmeetsingh3206/MovieRecommender"
+        githublink: "https://github.com/jasmeetsingh3206/MovieRecommender",
+        imgsrc:"https://i.ibb.co/MNjxWQK/Screen-Shot-2022-08-11-at-00-28-09.jpg"
     },
     {
         id:4,
@@ -42,7 +47,8 @@ export const user= {
         desc:"A sensor-based machine that calculates a person's Height, Weight, Temperature, Heart rate, BMI, and SpO2 using an 8- foot wooden frame and an ESP8266 MCU. The collected data is uploaded to the cloud in real-time and displayed on a web dashboard, providing users with instant access to their health metrics.",
         tech:["C++", "NodeMCU", "Iot", "Arduino", "Electronic sensors", "Thingsboard.io"],
         demolink: "https://demo.thingsboard.io/dashboard/71f6d200-18bd-11ed-9418-cd0267d90036?publicId=b03ac070-0bd6-11ec-9fa1-17067ddb76a8",
-        githublink: "https://github.com/jasmeetsingh3206/SmartHealth"
+        githublink: "https://github.com/jasmeetsingh3206/SmartHealth",
+        imgsrc: "https://i.ibb.co/wKwfnsF/githubmicrofit.png"
     }]
 
   }
--- src/components/Projects/newProjectCard.jsx
+++ src/components/Projects/newProjectCard.jsx
@@ -11,22 +11,24 @@ export default function NewCard(props) {
 
     function pillGenerator(tech) {
         return <span className=" italic bg-purple-300  inline-block m-1 py-1 px-2 rounded  text-sm">{tech}</span>;
-        
+
     };
     return (
-        <div className="flex flex-col justify-between w-full md:h-full rounded-lg  bg-slate-200 border border-gray-200 shadow  hover:shadow">
-            <img className="object-cover w-full rounded-t-lg h-96 md:h-auto  md:rounded-l-lg" src="https://cdn.dribbble.com/users/375995/screenshots/6451756/goldencanongrid.png" alt="projectImage" />
+        <div className="flex flex-col justify-between w-full md:h-full rounded-lg  bg-slate-200   shadow  hover:shadow">
+            <div className="p-3 pt-4 bg-gradient-to-b from-purple-400 to-slate-200 rounded-lg">
+                <img className="object-contain w-full rounded-t-lg h-80 md:h-auto  md:rounded-t-lg" src={props.project.imgsrc} alt="projectImage" />
+            </div>
             <div className="flex flex-col justify-between p-4 leading-normal">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{props.project.title}</h5>
-                <div className=" mb-3 ">{props.project.tech.map((skill)=>pillGenerator(skill))}</div>
+                <div className=" mb-3 ">{props.project.tech.map((skill) => pillGenerator(skill))}</div>
                 <p className="mb-3 font-normal text-gray-700">{props.project.desc}</p>
             </div>
             <div className="flex flex-row gap-3 m-1 mb-3 justify-center">
                 <a href={props.project.githublink} rel="noreferrer" target="_blank" class="text-white shadow-sm  hover:bg-purple-800 bg-purple-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-lg text-xl px-3 py-0 h-12 text-center inline-flex items-center ">
                     <GoRepo className="mr-1" /> GitHub
                 </a>
-                <a href={props.project.demolink} rel="noreferrer" target="_blank" type="button"  class="text-white hover:bg-purple-800 shadow-sm bg-purple-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-lg text-xl px-3 py-0 h-12 text-center inline-flex items-center ">
-                <CgWebsite className="mr-1" /> Demo
+                <a href={props.project.demolink} rel="noreferrer" target="_blank" type="button" class="text-white hover:bg-purple-800 shadow-sm bg-purple-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-lg text-xl px-3 py-0 h-12 text-center inline-flex items-center ">
+                    <CgWebsite className="mr-1" /> Demo
                 </a>
             </div>
         </div>
